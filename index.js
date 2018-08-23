49cbaae79c7036e4691ac350943cec2e71cf20dd
Kiki Dolan <cdolan323@gmail.com>
2018-08-23 22:30:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const element = document.getElementById('dodger')
 document.addEventListener('keydown', (event) => {
  console.log(event)
  if (event.which === 37 ) {
-    let previousLeft = element.style.left > 0
+    let previousLeft = element.style.left
     let previousLeftNumber = parseInt(previousLeft.replace('px', ""), 10)
     let newLeftNumber = (previousLeftNumber -1) + "px"
     element.style.left = newLeftNumber
