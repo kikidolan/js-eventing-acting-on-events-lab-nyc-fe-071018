c74e8b6f4d6f7e7cc0aef00222e4eb9bae27e9af
Kiki Dolan <cdolan323@gmail.com>
2018-08-23 22:16:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const element = document.getElementById('dodger')
 
 document.addEventListener('keydown', (event) => {
- console.log(event)if (event.which === 37) {
+ console.log(event)
+ if (event.which === 37) {
+   event.style.backgroundColor = 'yellow'
    
  }
 })
