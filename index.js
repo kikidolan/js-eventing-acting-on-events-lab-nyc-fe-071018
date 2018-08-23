77fe5a358f2e7cb35d9b10b04faf41d3a5e13bbc
Kiki Dolan <cdolan323@gmail.com>
2018-08-23 22:38:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ document.addEventListener('keydown', (event) => {
  let previousLeft = element.style.left
  let previousLeftNumber = parseInt(previousLeft.replace('px', ""), 10)
  if (previousLeftNumber > 0) {
-    let newLeftNumber = (previousLeftNumber -15) + "px"
+    let newLeftNumber = (previousLeftNumber -1) + "px"
     element.style.left = newLeftNumber
   }
  }
 })
+
+function moveDodgerRight(event) {
+  if (event.which === 39) {
+    let previousRight = element.style.left
+    let previousRightNumber=parseInt(previousRight.replace('px', ''),10)
+    if (previousRightNumber > 0 ) {
+      let newRightNumber = (previousRightNumer +1) + "px"
+      element.style.right = newRightNumber
+    }
+  }
+}
