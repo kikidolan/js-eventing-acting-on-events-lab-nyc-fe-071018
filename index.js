e884de148b3001dc20673a5accac318203e5bdca
Kiki Dolan <cdolan323@gmail.com>
2018-08-23 22:22:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ document.addEventListener('keydown', (event) => {
  console.log(event)
  if (event.which === 37) {
     let previousLeft = element.style.left
-    let previousLeftNumber = parseInt(previousLeft.replace('px', ""))
+    let previousLeftNumber = parseInt(previousLeft.replace('px', ""), 10)
+    console.log(previousLeftNumber)
  }
 })
